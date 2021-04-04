d766fd6c5fd079087d48c37b5ac8470af746fb59
Hugo Luis <hugoluis.system@gmail.com>
2021-04-03 23:48:35 -0500

    Add project files.

---
+++ EjerciciosModulo_1/Ejercicio1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+//Permite al usuario escribir 2 números y mostrar en pantalla cuál
+//de los dos es el más grande. Si son iguales, mostrar cualquiera.
+namespace EjerciciosModulo_1
+{
+    class Ejercicio1
+    {
+      
+    }
+}
---
+++ EjerciciosModulo_1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace EjerciciosModulo_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+
+           
+                try
+                {
+                    Console.WriteLine("Escriba un Número");
+                    int  numero1 = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Escriba un Número");
+                    int numero2 = int.Parse(Console.ReadLine());
+
+                    Console.WriteLine("El Número mayor es : ");
+                    if (numero1>numero2)
+                    {
+                        Console.WriteLine(numero1);
+                    }
+                    else
+                    {
+                        Console.WriteLine(numero2);
+                    }
+
+                    break;
+                }
+                catch(Exception ex)
+                {
+                    Console.WriteLine("Asegurese de escribir valores numericos");
+                }
+             }
+
+
+        }
+    }
+}
